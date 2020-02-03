72cc8ba28a772367d1fedb7a1fd7614a8ee49624
a_umut <askar.umut.kal@gmail.com>
2020-02-03 10:33:42 +0600

    header mobile

--- src/components/header/header__menu.vue
+++ src/components/header/header__menu.vue
@@ -1,7 +1,13 @@
 <template>
-    <ul class="f-menu f-header__menu">
-        <li class="f-menu__item" v-for="item in menuItems" :key="item"><a href="#" class="f-menu__link">{{item}}</a></li>
-    </ul>
+    <div class="f-menu_outer">
+        <button type="button" class="btn btn-gamburg" id="btn_gamburg"></button>
+        <div class="f-menu_wrap">
+            <button type="button" class="btn btn-close" id="btn_close"></button>
+            <ul class="f-menu f-header__menu">
+                <li class="f-menu__item" v-for="item in menuItems" :key="item"><a href="#" class="f-menu__link">{{item}}</a></li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
