b42e6b35e67f0c575423246566e1916796ca0337
a_umut <askar.umut.kal@gmail.com>
2020-02-03 10:36:33 +0600

    header mobile --merged

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <template>
   <div id="app">
     <Header/>
+    <banner-slider/>
   </div>
 </template>
 
 <script>
 import './assets/sass/style.sass'
 import Header from './components/Header'
+import BannerSlider from './components/BannerSlider'
 
 export default {
   name: 'app',
   components: {
-    Header
+    Header, BannerSlider
+  },
+  data() {
+    return {
+
+    }
   }
 }
 </script>
+
+<style scoped lang="sass">
+    .f-banner--1
+        background: #8E1C57
+    .f-banner--2
+        background: #4FB9E1
+    .f-banner--3
+        background: #6A24DB
+</style>
