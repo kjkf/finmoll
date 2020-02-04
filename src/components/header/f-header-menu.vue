af24d1347a255bb5ced1dcb407e9c33732badd2e
Lola <yakjkf@gmail.com>
2020-02-04 13:22:25 +0600

    data from file

--- src/components/header/f-header-menu.vue
+++ src/components/header/f-header-menu.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="f-menu_outer">
-        <button type="button" class="btn btn-gamburg" id="btn_gamburg"></button>
-        <div class="f-menu_wrap">
-            <button type="button" class="btn btn-close" id="btn_close"></button>
+        <button type="button" class="btn btn-gamburg" id="btn_gamburg" @click="toggleMenu"></button>
+        <div class="f-menu_wrap" :class="{'menuShow': isMenuShow}">
+            <button type="button" class="btn btn-close" id="btn_close" @click="closeMenu"></button>
             <ul class="f-menu f-header__menu">
                 <li class="f-menu__item" v-for="item in menuItems" :key="item"><a href="#" class="f-menu__link">{{item}}</a></li>
             </ul>
@@ -15,12 +15,23 @@
         name: "f-header-menu",
         data() {
             return {
-                menuItems: ['Рассрочка на покупки', 'Займ в сети kari', 'Займ на дому', 'Погашение']
+                menuItems: ['Рассрочка на покупки', 'Займ в сети kari', 'Займ на дому', 'Погашение'],
+                isMenuShow: false
+            }
+        },
+        methods: {
+            toggleMenu() {
+                this.isMenuShow = !this.isMenuShow;
+            },
+            closeMenu() {
+                this.isMenuShow = false;
             }
         }
     }
 </script>
 
 <style scoped>
-
+    .menuShow {
+        display: block;
+    }
 </style>
